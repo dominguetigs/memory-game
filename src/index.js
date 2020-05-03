c34c7b21620d9652ca5cf458da17ffa99273c808
Gustavo Santana Domingueti <gustavo.s.domingueti@gmail.com>
2020-05-03 14:41:56 -0300

    Ajuste de estilo do radio button.

--- src/index.js
+++ src/index.js
@@ -9,8 +9,12 @@ const gameStartButton = document.querySelector('.modal button');
 modalContainer.style.display = 'block';
 
 gameStartButton.addEventListener('click', e => {
+    const difficulty = document.querySelector('input[name="difficulty"]:checked').value;
+
     (new Draw(gameContainer, 'easy')).init();
+
     modalContainer.style.display = 'none';
     container.style.display = 'block';
-    (new Game('hard')).init();
+
+    (new Game(difficulty)).init();
 });
