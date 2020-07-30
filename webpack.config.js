bc07f3e0e0c14393f068a1b7839e492a4cdbc23f
Gustavo Domingueti <gustavo.s.domingueti@gmail.com>
2020-07-29 22:41:47 -0300

    :wrench: Reconfigura arquivos

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src', 'index.js'),
+    entry: path.resolve(__dirname, 'public', 'index.js'),
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, 'dist'),
